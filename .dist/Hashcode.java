29458fb4f9c93078d6d065d11a468979b6f3c80a
Samtha Stha <samshrestha990@gmail.com>
2025-12-27 22:28:11 +0545

    added type on Hashcode.java class by rajan kam chor

--- .dist/Hashcode.java
+++ .dist/Hashcode.java
@@ -3,12 +3,14 @@
     {
       String model;
       int price;
+      String type;
     }
 public class Hashcode {
     public static void main(String[] args) {
       Laptop obj = new Laptop();
       obj.model = "Lenovo Yoga";
       obj.price = 1000;
+      obj.type = "365";
 
       System.out.println(obj);
     }
