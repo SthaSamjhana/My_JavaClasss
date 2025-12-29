5483db6b7ad362bb06b4d4a144ce16f639de615b
Samtha Stha <samshrestha990@gmail.com>
2025-12-29 12:20:42 +0545

    final Update

--- .dist/Hashcode.java
+++ .dist/Hashcode.java
@@ -1,17 +1,37 @@
 
-    class Laptop
+class Laptop
     {
       String model;
       int price;
-      String type;
+      
+// object class method
+       public String toString()
+    {
+      return model + " :" + price ;
+    } 
+
+    public boolean equals(Laptop that)
+    {
+      if(this.model.equals(that.model) && this.price ==that.price)
+        return true;
+      else
+        return false;
     }
+  }
+
+   
 public class Hashcode {
     public static void main(String[] args) {
       Laptop obj = new Laptop();
       obj.model = "Lenovo Yoga";
       obj.price = 1000;
-      obj.type = "365";
 
-      System.out.println(obj);
+      Laptop obj1 = new Laptop();
+      obj1.model = "Lenovo Yoga";
+      obj1.price = 1000;
+      
+      boolean result = obj.equals(obj1);
+
+      System.out.println(result);
     }
 }
